8f27a6e8485dea3269bf4e60c8270061bd16f271
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-05-18 08:01:31 +0200

    Add component for storybook

---
+++ src/components/Map/Map.stories.js
@@ -0,0 +1,24 @@
+import Map from './Map';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+
+export default {
+	title: 'Components/Organizer',
+	component: Map,
+	decorators: [
+		Story => {
+			return (
+				<>
+					<div
+						id="cesiumContainer"
+						style={{ padding: '3em', width: '400px', height: '400px' }}
+					/>
+					<Story />
+				</>
+			);
+		},
+	],
+};
+
+export function Default() {
+	return <Map />;
+}
