9d938d9f467cba1a00caf4b2a93c358ccf472da3
Irene Papaspyratos <irene.papaspyratos@gmail.com>
2022-05-30 01:05:48 +0200

    Adjust component to routing

--- src/components/ElementList/ElementList.jsx
+++ src/components/ElementList/ElementList.jsx
@@ -1,14 +1,32 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+import urlAdjustLight from '../../../src/services/url-adjust-light';
 
 export default function ElementList({ elements, header }) {
+	const displayHeader = header.substr(0, 1).toUpperCase() + header.substr(1);
+	const urlPart = header.substr(0, header.length - 1);
+
 	return (
 		<section>
-			<h2>{header}</h2>
-			<ul>
-				{elements?.map(element => {
-					return <li key={element.id}>{element.name}</li>;
-				})}
-			</ul>
+			<details>
+				<summary>{displayHeader}</summary>
+				<ul>
+					{elements.map(element => {
+						return (
+							<li key={element.id}>
+								<Link
+									href={{
+										pathname: `/list/${urlPart}/[slug]`,
+										query: { slug: urlAdjustLight(element.name) },
+									}}
+								>
+									<a>{element.name}</a>
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</details>
 		</section>
 	);
 }
